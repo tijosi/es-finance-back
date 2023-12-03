83eb429b1f84eb9372cfa20c3e1bf013fba1d1b3
Edson Martins <edsondizeseis@hotmail.com>
2023-12-03 11:55:53 -0300

    pronto

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::any('/info', function (Request $request) {
     $var = 25;
-    phpinfo();
+    response()->json($var);
 });
 
 Route::get('/enum/{enumClass}', [Tenum::class, 'getEnumValues']);
